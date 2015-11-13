ca747cde54a786671db6d587b99587b0558ee78e
Kyle Hornberg <kyle.hornberg@gmail.com>
2015-11-13 12:41:13 -0600

    Try to get the correct Docker connection settings

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@
 
 "use strict()";
 
+var os = require('os');
+var execSync = require('child_process').execSync;
 var Docker       = require('dockerode');
 var fs           = require('fs');
 var DockerEvents = require('docker-events');
@@ -34,6 +36,29 @@ emitter.on("disconnect", (message) => console.log('disconnect', message));
 // when starting from app doesn't get envvars because docker machine has not added them
 // nor when starting from a shell with out the docker command run already
 function connect() {
+  if (os.platform != 'Linux') {
+    var output = execSync('docker-machine env default').toString();
+    var lines = output.split('\n');
+    var docker_machine = {};
+    lines.forEach((line) => {
+      x = line.split('=');
+      if (x[0].indexOf('export') >= 0) {
+        docker_machine[x[0].split(' ')[1]] = x[1].replace(/"/g, '');
+      }
+    });
+
+    var ip = docker_machine['DOCKER_HOST'].match(/\w*:\/\/(.*):/)[1];
+    var port = docker_machine['DOCKER_HOST'].match(/\w*:\/\/.*:(.*)/)[1];
+
+    return new Docker({
+      host: ip,
+      port: port,
+      ca: fs.readFileSync(`${docker_machine['DOCKER_CERT_PATH']}/ca.pem`),
+      cert: fs.readFileSync(`${docker_machine['DOCKER_CERT_PATH']}/cert.pem`),
+      key: fs.readFileSync(`${docker_machine['DOCKER_CERT_PATH']}/key.pem`),
+    });
+  }
+  // assumes a local docker connection
   return new Docker();
 }
 
