4b3393c118477df2d20ac0b68cb1ab3c7604362e
Kyle Hornberg <kyle.hornberg@gmail.com>
2016-05-09 16:11:19 -0500

    Add default support for native Docker app
    By default, the native docker app on OS X will have a socket. If one
    wants to use that, remove the `.docker-indicator` file.

--- src/connect.js
+++ src/connect.js
@@ -3,15 +3,20 @@ var fs       = require('fs');
 var userHome = require('user-home');
 var read     = require('read-yaml');
 var Docker   = require('dockerode');
+var fileExists = require('file-exists');
 
 module.exports = (function () {
-  var config = read.sync(`${userHome}/.docker-indicator.yaml`);
-  return new Docker({
-    protocol: config.docker.protocol,
-    host: config.docker.ip,
-    port: config.docker.port,
-    ca: fs.readFileSync(config.docker.ca),
-    cert: fs.readFileSync(config.docker.cert),
-    key: fs.readFileSync(config.docker.key),
-  });
-})()
+  var configFile = `${userHome}/.docker-indicator.yaml`;
+  if (fileExists(configFile)) {
+    var config = read.sync(configFile);
+    return new Docker({
+      protocol: config.docker.protocol,
+      host: config.docker.ip,
+      port: config.docker.port,
+      ca: fs.readFileSync(config.docker.ca),
+      cert: fs.readFileSync(config.docker.cert),
+      key: fs.readFileSync(config.docker.key),
+    });
+  }
+  return new Docker({socketPath: '/var/tmp/docker.sock'});
+})();
